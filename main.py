6ca06427afab1659ce07b681ae820bbf78995986
Paul De Palma <depalma@gonzaga.edu>
2019-04-05 17:44:59 -0700

    added menu funtions

--- main.py
+++ main.py
@@ -14,10 +14,34 @@ def main(filename):
         file_stream.close()
     except FileNotFoundError:
         sys.exit('invalid filename %s' % filename)
+    userChoice = 0
+    while(userChoice != 3):
+        print("What do you want to do?")
+        print("1) Check if user exists")
+        print("2) Check connection between users")
+        print("3) Quit")
+        userChoice = input()
+        #just test cases, replace with actual params
+        doChoice(userChoice, {'a': 0, 'b' : 1}, [{'b':10},{'a':5}])
 
 # reads file
 def read_file(filename):
     print("read file")
+
+def doChoice(userChoice, userDictionary, graph):
+    if userChoice == 1:
+        checkName = raw_input("Enter the users name > ")
+        print(checkName)
+        if checkName in userDictionary:
+            print(checkName + " exists")
+        else:
+            print(checkName + " does not exist")
+    elif userChoice == 2:
+        names = raw_input("What users (seperated by spaces) > ")
+        names = names.split()
+        weight = graph[userDictionary[names[0]]][names[1]]
+        print("The connection from " + names[0] + " to " + names[1] + " has weight " + str(weight))
+
 
 # takes in command line arguments to execute program
 if __name__ == '__main__':
