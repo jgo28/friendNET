495a4e8d6f865c267df4142f95fa858b0c25b104
jgo98 <gojoshua98@gmail.com>
2019-04-05 17:10:23 -0700

    Base code for reading from a file

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-# 
-#
-#
+# Authors: Joshua Go, Evan Swanson
+# Course: CPSC 450
+# Assignment: Final Project
+# Description:
+#   
+# ----------------------------------------------------------------------
+
+import sys
+
+def main(filename):
+    try:
+        file_stream = open(filename, 'r')
+        read_file(file_stream)
+        file_stream.close()
+    except FileNotFoundError:
+        sys.exit('invalid filename %s' % filename)
+
+# reads file
+def read_file(filename):
+    print("read file")
+
+# takes in command line arguments to execute program
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit('Usage: %s file' % sys.argv[0])
+    main(sys.argv[1])
