87f5ea119ea1b3ea7d318f3da2eb9878d3e957f4
jgo98 <gojoshua98@gmail.com>
2019-04-05 17:21:06 -0700

    adjacency list implementation

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import sys
 def main(filename):
     try:
         file_stream = open(filename, 'r')
-        read_file(file_stream)
+        read_file(file_stream) 
         file_stream.close()
     except FileNotFoundError:
         sys.exit('invalid filename %s' % filename)
@@ -19,6 +19,9 @@ def main(filename):
 # reads file
 def read_file(filename):
     print("read file")
+    adj_list = []
+    for i in filename.read().split(): 
+        
 
 # takes in command line arguments to execute program
 if __name__ == '__main__':
